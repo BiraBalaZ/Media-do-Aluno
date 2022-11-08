fd085a6de9f5bb9e26a6b2c9fa0c39ebd3df8ff3
BiraBalaZ <85596186+BiraBalaZ@users.noreply.github.com>
2022-11-08 19:05:39 -0300

    Update 4.1
    Formatando o código

--- media_do_aluno4.0.py
+++ media_do_aluno4.0.py
@@ -4,9 +4,9 @@ import random
 
 #Coletando nome e idade do aluno
 nome    = str(input('Digite aqui o primeiro nome do aluno: \n >>>'))
-idade   = int(input(f'Qual a idade de {nome}: \n >>>'))
-altura  = float(input(f'Qual a altura de {nome}: \n >>>'))
-peso    = float(input(f'Qual o peso de {nome}: \n >>>'))
+idade   = int(input(f'\nQual a idade de {nome}: \n >>>'))
+altura  = float(input(f'\nQual a altura de {nome}: \n >>>'))
+peso    = float(input(f'\nQual o peso de {nome}: \n >>>'))
 id      = random.randint(100000, 999999)
 
 #Calculando o IMC do Aluno
@@ -17,9 +17,11 @@ coluna = 'Dados'.split()
 linha  = 'Nome Idade Altura Peso ID IMC'.split()
 dados  = f'{nome} {idade} {altura} {peso} {id} {imc}'.split()
 
+#Criando a tabela
 tabela = pd.DataFrame(data = dados, index = linha, columns = coluna)
 
-print(tabela)
+#Printando a tabela depois de coletar os dados do usuário
+print(f'\n{tabela}')
 
 #Exibindo mensagem de acordo com o IMC do Aluno
 if imc < 18.5:
@@ -32,31 +34,31 @@ elif imc >= 30.0:
 	print(f'\n{nome} está muito acima do peso.\n')
 
 #Coletando as notas de cada matéria do aluno
-n1 = float(input(f'Digite a nota de Português do aluno {nome}   \n >>>'))
-n2 = float(input(f'Digite a nota de Matemática do aluno {nome}  \n >>>'))
-n3 = float(input(f'Digite a nota de Química do aluno {nome}     \n >>>'))
-n4 = float(input(f'Digite a nota de História do aluno {nome}    \n >>>'))
-n5 = float(input(f'Digite a nota de Geografia do aluno {nome}   \n >>>'))
-n6 = float(input(f'Digite a nota de Física do aluno {nome}      \n >>>'))
-n7 = float(input(f'Digite a nota de Inglês do aluno {nome}      \n >>>'))
-n8 = float(input(f'Digite a nota de Artes do aluno {nome}       \n >>>'))
-n9 = float(input(f'Digite a nota de Ed. Física do aluno {nome}  \n >>>'))
-n10= float(input(f'Digite a nota de Espanhol do aluno {nome}    \n >>>'))
+n1 = float(input(f'Digite a nota de Portguês do aluno {nome}      \n >>>'))
+n2 = float(input(f'\nDigite a nota de Matemática do aluno {nome}  \n >>>'))
+n3 = float(input(f'\nDigite a nota de Química do aluno {nome}     \n >>>'))
+n4 = float(input(f'\nDigite a nota de História do aluno {nome}    \n >>>'))
+n5 = float(input(f'\nDigite a nota de Geografia do aluno {nome}   \n >>>'))
+n6 = float(input(f'\nDigite a nota de Física do aluno {nome}      \n >>>'))
+n7 = float(input(f'\nDigite a nota de Inglês do aluno {nome}      \n >>>'))
+n8 = float(input(f'\nDigite a nota de Artes do aluno {nome}       \n >>>'))
+n9 = float(input(f'\nDigite a nota de Ed. Física do aluno {nome}  \n >>>'))
+n10= float(input(f'\nDigite a nota de Espanhol do aluno {nome}    \n >>>'))
 
 #Somando as notas e dividindo por 10, para ter a média do aluno
 media = float((n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10) / 10)
 
 #Imprimindo o nome, idade e média baseada no cálculo acima
-print(f'Com as notas fornecidas, a média do aluno {nome} de {idade} anos é {media:.2f}')
+print(f'\nCom as notas fornecidas, a média do aluno {nome} de {idade} anos é {media:.2f}')
 
 #Se a média for 6 ou acima, ele passou;
 #Se a média for 5 ele passou, mas precisa melhorar;
 #Se a média for 4 ou abaixo, ele reprovou.
 if media >= 6.0 and media <= 10:
-	print(f'\n{nome} foi APROVADO com SUCESSO!\n')
+	print(f'{nome} foi APROVADO com SUCESSO!\n')
 elif media == 5.0:
-	print(f'\n{nome} foi aprovado, mas pode melhorar\n')
+	print(f'{nome} foi aprovado, mas pode melhorar\n')
 elif media <= 4.0 and media >= -5.0:
-	print(f'\n{nome} foi REPROVADO, precisa se empenhar mais\n')
+	print(f'{nome} foi REPROVADO, precisa se empenhar mais\n')
 else:
     print('[ERROR 001] Erro desconhecido, favor contatar o Dev encarregado')
